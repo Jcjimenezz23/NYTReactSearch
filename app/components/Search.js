abd9c772576b6eb620b7fa24c6603a06b5c10a1d
Jc <jcjimenezz23@gmail.com>
2017-08-21 21:23:27 -0400

    built query.js

---
+++ app/components/Search.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import helpers from "../untils/helpers";
+
+
+class Search extends React.Component {
+
+	constructor(props) {
+
+		super(props);
+
+		this.state = {
+			results: {}
+		}
+	}
+
+	setQuery (newQuery, newStart, newEnd) {
+		helpers.runQuery(newQuery, newStart, newEnd).then(function(data) {
+			this.setState({results: {docs: data.docs}})
+		}.bind(this));
+	}
+
+	render() {
+		return (
+			<div className= "main-container">
+				<Query setQuery={this.setQuery()} />
+				<Results results={this.state.results} />
+			</div>
+		)
+	}
+}
+
+export default Search;
