efe4a576d47e3b0fcacb43f6d5447a07de49dee9
Jc <jcjimenezz23@gmail.com>
2017-08-23 19:38:09 -0400

    made git ignore file

--- app/components/Search/Query.js
+++ app/components/Search/Query.js
@@ -1,17 +1,15 @@
-import React from "react";
+var React = require("react");
+var Router = require("react-router");
 
-class Query extends React.component {
-
-		constructor(props) {
-			
-			super(props);
-
-			this.state = {
-				search: "",
-				start: "",
-				end: ""
+var Query = React.createClass({
+	mixins: [Router.History],
+	getInitialState: function(){
+		return{
+			"term": "",
+			"start": "",
+			"end": ""
 		}
-	}
+	},
 
 	handleSubmit (event) {
 		event.preventDefault();
@@ -27,10 +25,38 @@ class Query extends React.component {
 		this.props.setQuery(this.state.search, this.state.start, this.state.end)
 	}
 
-	render() {
-		return (
-			<div>
-				<h1>This is the Query</h1>
+	render: function(){
+		return(
+			<div className="main-container">
+				<div className="row">
+				<div className="col-lg-12">
+				<div className="panel panel-primary">
+			  		<div className="panel-heading">
+			    		<h1 className="lead">
+			    			<span class="glyphicon glyphicon-search"></span>  Search Query
+			    		</h1>
+			  		</div>
+			  		<div className="panel-body">
+						<form>
+							<div className="form-group">
+								<label for="term">  <h4 className="">Keyword: </h4></label>
+								<input type="text" value={this.state.value} className="form-control" id="term" onChange={this.handleChange} required />
+							</div>
+							<div className="form-group">
+								<label for="start"> <h4> Start Year: </h4></label>
+								<input type="number" value={this.state.value} className="form-control" id="start" onChange={this.handleChange} required />
+							</div>
+							<div className="form-group">
+								<label for="end"><h4 className=""> End Year: </h4> </label>
+								<input type="text" value={this.state.value} className="form-control" id="end" onChange={this.handleChange} required />
+							</div>
+							<button type="button" className="btn btn-default" onClick={this.handleSubmit}> <h4> <span class="glyphicon glyphicon-search"></span>  Search </h4>
+							</button>
+						</form>
+				  	</div>
+				</div>
+				</div>
+				</div>
 			</div>
 		)
 	}
